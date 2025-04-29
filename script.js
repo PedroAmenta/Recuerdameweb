0160e8ba48205da343a5cf19ae0a641cd352d109
PedroAmenta <104359639+PedroAmenta@users.noreply.github.com>
2025-04-28 21:33:14 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+// Navegación por secciones
+document.querySelectorAll("nav ul li a").forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    const destino = link.getAttribute("href").substring(1);
+
+    document.querySelectorAll(".seccion").forEach(seccion => {
+      seccion.classList.remove("activa");
+    });
+
+    const seccionActiva = document.getElementById(destino);
+    if (seccionActiva) {
+      seccionActiva.classList.add("activa");
+      window.scrollTo(0, 0);
+    }
+  });
+});
+
+function irAProyecto() {
+  document.querySelectorAll(".seccion").forEach(seccion => {
+    seccion.classList.remove("activa");
+  });
+  const seccionProyecto = document.getElementById("proyecto");
+  seccionProyecto.classList.add("activa");
+  window.scrollTo(0, 0);
+}
+
+function irAFadesa() {
+  const url ="https://fadesa.org/"; // Cambia esto por el enlace al que deseas redirigir
+  window.location.href = url; // Redirige al enlace especificado
+  window.open(url, "_blank");
+}
+
+
+
+
+
+
+
